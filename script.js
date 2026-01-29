09c6274f4901e93d4d8d780719da660f05ed96f6
Prriiitttt <pritjpatel3501@gmail.com>
2026-01-28 21:27:19 -0500

    Changes in HTML and JS file

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Checking 1 2 3..")
+const cityh1 = document.querySelector(".city");
+const currentTemp = document.querySelector(".current-temp");
+const cityInput = document.querySelector(".city-search");
+const searchBtn = document.querySelector(".search-btn");
+
+searchBtn.addEventListener("click", () => {
+  if (cityInput.value == "") {
+    alert("Please enter city name");
+  } else {
+    console.log(cityInput.value);
+    cityInput.value = '';
+  }
+});
